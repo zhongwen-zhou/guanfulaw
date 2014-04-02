252a0868b80751a2a855b0acebc6082766968cc9
majianguang <majianguang@gmail.com>
2014-04-03 01:09:34 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	$("#home").on('click',function(){
+		window.location.href="index.html";
+	});
+	$("#about").on('click',function(){
+		window.location.href="about.html";
+	});
+	$("#contact").on('click',function(){
+		window.location.href="contact.html";
+	});
+
+});
