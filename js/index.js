98daf069c60694c92c9ebc39acd833d582a0a4be
majianguang <majianguang@gmail.com>
2014-04-04 01:23:54 +0800

    update

--- js/index.js
+++ js/index.js
@@ -9,4 +9,30 @@ $(function(){
 		window.location.href="contact.html";
 	});
 
+	$("#team").on('click',function(){
+		alert(0);
+		window.location.href="team.html";
+	});
+
+
+	$("#baowangao").on('click',function(){
+		window.location.href="baowangaolawyer.html";
+	});
+
+	$("#jiangdeyun").on('click',function(){
+		window.location.href="jiangdeyunlawyer.html";
+	});
+
+	$("#huangpeng").on('click',function(){
+		window.location.href="huangpenglawyer.html";
+	});
+
+	$("#wenwei").on('click',function(){
+		window.location.href="wenweilawyer.html";
+	});
+
+	$("#jiangyu").on('click',function(){
+		window.location.href="jiangyulawyer.html";
+	});
+
 });
