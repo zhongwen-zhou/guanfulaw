5894dad76215a896de72280743073db58e2eb7a2
zhongwen <zhongwenzhou.wolf@gmail.com>
2014-07-04 16:13:53 +0800

    初始化问题

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+	def index
+	end
+
+	def about
+	end
+
+	def team
+	end
+
+	def service
+	end
+
+	def classiccase
+	end
+
+	def contact
+	end
+
+	def dynamic
+	end
+end
